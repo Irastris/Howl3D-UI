732325a6bc6c13d7ee47c71ba5c6bf507d34dae8
Irastris <irastris15@gmail.com>
2025-11-03 18:29:18 -0500

    Make modal closable

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Howl3D;
 
@@ -7,5 +8,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        CloseModalButton.Click += CloseModalButton_Click;
+    }
+    
+    private void CloseModalButton_Click(object sender, RoutedEventArgs e)
+    {
+        ModalContainer.IsVisible = false;
     }
 }
